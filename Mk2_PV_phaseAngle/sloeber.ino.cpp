931d45abd8500b3a4eefe4ebb9ac928556eb9208
Tomas Martišius <tomas@puga.vdu.lt>
2018-08-05 22:20:15 +0300

    Auto updates.

--- Mk2_PV_phaseAngle/sloeber.ino.cpp
+++ Mk2_PV_phaseAngle/sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-08-05 11:19:30
+//This file has been generated on 2018-08-05 17:16:55
 
 #include "Arduino.h"
 #include "Arduino.h"
@@ -10,9 +10,11 @@
 void setup() ;
 ISR(ADC_vect) ;
 void loop() ;
+void calculateEnergyInBucket(unsigned char phase, float realEnergy) ;
 void calculateFiringDelay(byte phase) ;
 void phaseAngleTriacControl() ;
 void processSamplePair(byte phase) ;
+void controlPhaseSwichRellay(unsigned char phase, float realPower) ;
 void checkLedStatus() ;
 
 #include "Mk2_PV_phaseAngle.ino"
